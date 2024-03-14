6fc6d32b68b91a48c461c68d3313c1453eb377d9
arrowgtr <123334721+arrowgtr@users.noreply.github.com>
2024-03-15 00:53:47 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,2 +1,24 @@
 // JavaScript functionality goes here
 // For example, fetching data from the backend, handling user interactions, etc.
+const loginBtn = document.getElementById("loginBtn");
+const loginPopup = document.getElementById("loginPopup");
+const registerLink = document.getElementById("registerLink");
+const registerPopup = document.getElementById("registerPopup");
+const closeBtns = document.querySelectorAll(".close");
+
+loginBtn.addEventListener("click", () => {
+  loginPopup.style.display = "block";
+});
+
+registerLink.addEventListener("click", (e) => {
+  e.preventDefault();
+  loginPopup.style.display = "none";
+  registerPopup.style.display = "block";
+});
+
+closeBtns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    loginPopup.style.display = "none";
+    registerPopup.style.display = "none";
+  });
+});
